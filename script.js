3ecaa532307e1702eda9060e6d69eb6c1e8429ac
man0o <148257137+man0o@users.noreply.github.com>
2023-12-23 20:47:10 +0400

    js task1

---
+++ script.js
@@ -0,0 +1,74 @@
+let number = 100;
+
+// if (number < 50) {
+//     console.log('ნაკლებია 50-ზე')
+// } else if (number > 20) {
+//     console.log('მეტია 20 ზე')
+// } else {
+//     console.log('შეცდომა')
+// };
+
+let newIf = 
+number < 50 
+? "მეტია 20 ზე"
+: "შეცდომა";
+
+console.log(number)
+
+
+        // 2
+
+ let name = "mariam"
+
+//  if (name == "mariam") {
+
+//     console.log(true)
+
+//  }else {
+
+//     console.log(false)
+//  }
+
+ let newIf2 = 
+
+ name == "mariam"
+ ? true
+ : false;
+
+
+ console.log(name)
+
+
+//  3
+
+
+let name1 = "mariam"
+
+switch (name1) {
+    case ("mariam"):
+    console.log(true)
+    break;
+
+    default :
+    console.log(false);
+}
+
+// 4
+
+
+const evenOrOdd = prompt("Enter a number: ");
+
+//if the number is even
+if(evenOrOdd % 2 == 0) {
+    console.log("The number is even.");
+}
+
+// if the number is odd
+else {
+    console.log("The number is odd.");
+};   
+
+
+
+
+
